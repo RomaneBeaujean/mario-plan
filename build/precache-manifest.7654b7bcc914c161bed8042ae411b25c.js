9e860db9da1830f2bb61b8d75feb0d6ed42159a9
Romane Beaujean <beaujean.romane@gmail.com>
2020-10-20 09:46:31 +0200

    20 octobre 2020

---
+++ build/precache-manifest.7654b7bcc914c161bed8042ae411b25c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412b97860161bb489e5a3a6cc184844b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c628db546f19e80e2125",
+    "url": "/static/css/main.0ac33894.chunk.css"
+  },
+  {
+    "revision": "1990eacf7fc9ba63e225",
+    "url": "/static/js/2.6541601b.chunk.js"
+  },
+  {
+    "revision": "94efaf9ef8dfdcc9b58d075c8086b235",
+    "url": "/static/js/2.6541601b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c628db546f19e80e2125",
+    "url": "/static/js/main.b816e05b.chunk.js"
+  },
+  {
+    "revision": "acdf868312f78a97cdd0",
+    "url": "/static/js/runtime-main.794c9aa6.js"
+  }
+]);
